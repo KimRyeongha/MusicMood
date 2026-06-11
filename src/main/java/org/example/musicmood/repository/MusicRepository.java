affa89f9b2c64900d2abc2ce90f2d206765effe6
KimRyeongha <krrhh22@gmail.com>
2026-06-11 10:43:37 +0900

    주석 편집

--- src/main/java/org/example/musicmood/repository/MusicRepository.java
+++ src/main/java/org/example/musicmood/repository/MusicRepository.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 @Repository
 public interface MusicRepository extends JpaRepository<Music, Long> {
-    // 나중에 특정 감성 태그(#새벽, #비오는날)만 검색하고 싶을 때 쓸 마법의 코드!
     List<Music> findByMoodTag(String moodTag);
     List<Music> findAllByLoginId(String loginId);
 }
